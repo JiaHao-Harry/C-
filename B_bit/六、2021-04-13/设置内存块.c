b652b4dd9bd6986857f7832f276910a98e76fb18
嘉豪 <89603692+JiaHao-Harry@users.noreply.github.com>
2021-12-23 23:45:14 +0800

    Add files via upload

---
+++ B_bit/六、2021-04-13/设置内存块.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char arr[] = "hello bit";
+	memset(arr, 'x', 5);
+
+	printf("%s\n", arr);
+
+	return 0;
+}
